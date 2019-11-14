a8967fef5e735e130e40aaaabb13ef91637e1a85
Thomas Hansen <thomas@gaiasoul.com>
2019-11-14 17:42:32 +0200

    Implementing

--- magic.lambda.scheduler/utilities/BackgroundService.cs
+++ magic.lambda.scheduler/utilities/BackgroundService.cs
@@ -3,12 +3,28 @@
  * See the enclosed LICENSE file for details.
  */
 
-using magic.node;
-using magic.signals.contracts;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 
 namespace magic.lambda.scheduler
 {
-    public class BackgroundService
+    public class BackgroundService : IHostedService, IDisposable
     {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
