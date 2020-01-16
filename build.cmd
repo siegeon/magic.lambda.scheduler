0178c5c9874ef8b661d04bb8e59c0e0e3bf5405a
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 17:26:42 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,8 @@
+
+set version=%1
+set key=%2
+
 cd %~dp0
+
 dotnet build magic.lambda.scheduler/magic.lambda.scheduler.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet nuget push magic.lambda.scheduler/bin/Release/magic.lambda.scheduler.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
