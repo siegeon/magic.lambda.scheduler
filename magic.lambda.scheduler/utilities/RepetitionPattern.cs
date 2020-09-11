7a0719ec5eb049522744ecd9b13db0fe6991fdb9
Thomas Hansen <thomas@gaiasoul.com>
2020-09-11 12:29:35 +0300

    Update RepetitionPattern.cs

--- magic.lambda.scheduler/utilities/RepetitionPattern.cs
+++ magic.lambda.scheduler/utilities/RepetitionPattern.cs
@@ -19,7 +19,7 @@ namespace magic.lambda.scheduler.utilities
         readonly int[] _day;
         readonly int? _hour;
         readonly int? _minute;
-        readonly int? _seconds;
+        readonly int? _second;
         readonly DayOfWeek[] _weekdays;
         readonly string _entity;
         readonly int _interval;
@@ -35,54 +35,20 @@ namespace magic.lambda.scheduler.utilities
             if (entities.Length == 2)
             {
                 // Interval type of repetition pattern.
-                switch(entities[1])
-                {
-                    case "seconds":
-                    case "minutes":
-                    case "hours":
-                    case "days":
-                    case "weeks":
-                    case "months":
-                        break;
-                    default:
-                        throw new ArgumentException("You can only use seconds, minutes, hours, days, weeks and months in an interval type of repetition pattern.");
-                }
-                _entity = entities[1];
-                _interval = int.Parse(entities[0]);
+                var result = CreateIntervalPattern(entities);
+                _entity = result.Entity;
+                _interval = result.Interval;
             }
             else
             {
                 // Every n'th type of repetition pattern.
-                if (entities.Length != 6)
-                    throw new ArgumentException("A repetition pattern must contain 6 or 2 entities separated by '.'");
-                if (entities[0] != "**")
-                    _month = entities[0].Split('|').Select(x => int.Parse(x)).ToArray();
-                if (entities[1] != "**")
-                    _day = entities[1].Split('|').Select(x => int.Parse(x)).ToArray();
-                if (entities[2] != "**")
-                    _hour = int.Parse(entities[2]);
-                if (entities[3] != "**")
-                    _minute = int.Parse(entities[3]);
-                if (entities[4] != "**")
-                    _seconds = int.Parse(entities[4]);
-                if (entities[5] != "**")
-                {
-                    // Weekday repetition pattern.
-                    var days = entities[5].Split('|');
-                    _weekdays = days.Select(x => (DayOfWeek)Enum.Parse(typeof(DayOfWeek), x, true)).ToArray();
-                    if (_month != null || _day != null)
-                        throw new ArgumentException("You cannot combine weekdays repetition pattern with month and day pattern");
-                    if (_hour == null || _minute == null || _seconds == null)
-                        throw new ArgumentException("A weekday pattern must be combined with hour, minutes and seconds");
-                }
-                else
-                {
-                    // Month/Day repetition pattern.
-                    if (_hour == null || _minute == null || _seconds == null)
-                        throw new ArgumentException("A month/day pattern must be combined with hour, minutes and seconds");
-                    if (_day == null)
-                        throw new ArgumentException("A month/day pattern must have at least a day value");
-                }
+                var result = CreateTimeStampPattern(entities);
+                _month = result.Months;
+                _day = result.Days;
+                _hour = result.Hour;
+                _minute = result.Minute;
+                _second = result.Second;
+                _weekdays = result.Weekdays;
             }
         }
 
@@ -103,52 +69,140 @@ namespace magic.lambda.scheduler.utilities
             result = new DateTime(result.Year, result.Month, result.Day, result.Hour, result.Minute, result.Second, 0);
             if (_entity == null)
             {
-                // Every n'th repetition pattern.
-                if (_weekdays != null)
+                return CalculateNextDateTime(result);
+            }
+            else
+            {
+                return CalculateNextIntervalDate(result);
+            }
+        }
+
+        #region [ -- Private helper methods -- ]
+
+        /*
+         * Creates an interval type of repetition pattern, such as "5.seconds", etc.
+         */
+        (string Entity, int Interval) CreateIntervalPattern(string[] entities)
+        {
+            // Interval type of repetition pattern.
+            switch(entities[1])
+            {
+                case "seconds":
+                case "minutes":
+                case "hours":
+                case "days":
+                case "weeks":
+                case "months":
+                    break;
+                default:
+                    throw new ArgumentException("You can only use seconds, minutes, hours, days, weeks and months in an interval type of repetition pattern.");
+            }
+            return (entities[1], int.Parse(entities[0]));
+        }
+
+        /*
+         * Creates a timestamp type of repetition pattern, such as "**.5|15.23.59.11.**", etc.
+         */
+        (int[] Months, int[] Days, int? Hour, int? Minute, int? Second, DayOfWeek[] Weekdays) CreateTimeStampPattern(string[] entities)
+        {
+            // Every n'th type of repetition pattern.
+            if (entities.Length != 6)
+                throw new ArgumentException("A repetition pattern must contain 6 or 2 entities separated by '.'");
+
+            int[] months = null;
+            int[] days = null;
+            int? hour = null;
+            int? minute = null;
+            int? second = null;
+            DayOfWeek[] weekdays = null;
+
+            if (entities[0] != "**")
+                months = entities[0].Split('|').Select(x => int.Parse(x)).ToArray();
+
+            if (entities[1] != "**")
+                days = entities[1].Split('|').Select(x => int.Parse(x)).ToArray();
+
+            if (entities[2] != "**")
+                hour = int.Parse(entities[2]);
+
+            if (entities[3] != "**")
+                minute = int.Parse(entities[3]);
+
+            if (entities[4] != "**")
+                second = int.Parse(entities[4]);
+
+            if (entities[5] != "**")
+            {
+                // Weekday repetition pattern.
+                if (months != null || days != null)
+                    throw new ArgumentException("You cannot combine weekdays repetition pattern with month and day pattern");
+                if (hour == null || minute == null || second == null)
+                    throw new ArgumentException("A weekday pattern must be combined with hour, minutes and seconds");
+                weekdays = entities[5]
+                    .Split('|')
+                    .Select(x => (DayOfWeek)Enum.Parse(typeof(DayOfWeek), x, true)).ToArray();
+            }
+            else
+            {
+                // Month/Day repetition pattern.
+                if (hour == null || minute == null || second == null)
+                    throw new ArgumentException("A month/day pattern must be combined with hour, minutes and seconds");
+                if (days == null)
+                    throw new ArgumentException("A month/day pattern must have at least a day value");
+            }
+            return (months, days, hour, minute, second, weekdays);
+        }
+
+        DateTime CalculateNextDateTime(DateTime result)
+        {
+            // Every n'th repetition pattern.
+            if (_weekdays != null)
+            {
+                // Weekday pattern.
+                result = new DateTime(result.Year, result.Month, result.Day, _hour.Value, _minute.Value, _second.Value);
+                while (true)
                 {
-                    // Weekday pattern.
-                    result = new DateTime(result.Year, result.Month, result.Day, _hour.Value, _minute.Value, _seconds.Value);
-                    while(true)
-                    {
-                        if (result > DateTime.UtcNow && _weekdays.Any(x => x == result.DayOfWeek))
-                            return result;
-                        result = result.AddDays(1);
-                    }
-                }
-                else
-                {
-                    // Month/Day pattern.
-                    result = new DateTime(result.Year, result.Month, result.Day, _hour.Value, _minute.Value, _seconds.Value);
-                    while(true)
-                    {
-                        if (result > DateTime.UtcNow &&
-                            (_month == null || _month.Any(x => result.Month == x)) &&
-                            _day.Any(x => result.Day == x))
-                            return result;
-                        result = result.AddDays(1);
-                    }
+                    if (result > DateTime.UtcNow && _weekdays.Any(x => x == result.DayOfWeek))
+                        return result;
+                    result = result.AddDays(1);
                 }
             }
             else
             {
-                switch(_entity)
+                // Month/Day pattern.
+                result = new DateTime(result.Year, result.Month, result.Day, _hour.Value, _minute.Value, _second.Value);
+                while (true)
                 {
-                    case "seconds":
-                        return result.AddSeconds(_interval);
-                    case "minutes":
-                        return result.AddMinutes(_interval);
-                    case "hours":
-                        return result.AddHours(_interval);
-                    case "days":
-                        return result.AddDays(_interval);
-                    case "weeks":
-                        return result.AddDays(_interval * 7);
-                    case "months":
-                        return result.AddMonths(_interval);
-                    default:
-                        throw new ArgumentException("You cannot possibly have reached this code!");
+                    if (result > DateTime.UtcNow &&
+                        (_month == null || _month.Any(x => result.Month == x)) &&
+                        _day.Any(x => result.Day == x))
+                        return result;
+                    result = result.AddDays(1);
                 }
             }
         }
+
+        DateTime CalculateNextIntervalDate(DateTime result)
+        {
+            switch (_entity)
+            {
+                case "seconds":
+                    return result.AddSeconds(_interval);
+                case "minutes":
+                    return result.AddMinutes(_interval);
+                case "hours":
+                    return result.AddHours(_interval);
+                case "days":
+                    return result.AddDays(_interval);
+                case "weeks":
+                    return result.AddDays(_interval * 7);
+                case "months":
+                    return result.AddMonths(_interval);
+                default:
+                    throw new ArgumentException("You cannot possibly have reached this code!");
+            }
+        }
+
+        #endregion
     }
 }
